eac3b6ab0ef91bed9757f6f6266bbb5224102f2e
Luis Fernando <guffenix@gmail.com>
2023-11-11 23:53:45 -0500

    feat: findById request using tdd

--- onecard/src/main/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashController.java
+++ onecard/src/main/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashController.java
@@ -3,16 +3,17 @@ package solutions.mundovirtual.cash_card.infraestructure.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import solutions.mundovirtual.cash_card.domain.CashCard;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CardCashController {
 
 	@GetMapping("/{requestedId}")
-	public ResponseEntity<String> findById() {
-		return ResponseEntity.ok("{}");
+	public ResponseEntity<CashCard> findById() {
+		CashCard cashCard = new CashCard(99L, 123.45);
+		return ResponseEntity.ok(cashCard);
 	}
 }
--- onecard/src/test/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashControllerTest.java
+++ onecard/src/test/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashControllerTest.java
@@ -1,8 +1,5 @@
 package solutions.mundovirtual.cash_card.infraestructure.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
@@ -24,5 +21,12 @@ final class CardCashControllerTest {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isEqualTo(99);
+
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
 	}
 }
