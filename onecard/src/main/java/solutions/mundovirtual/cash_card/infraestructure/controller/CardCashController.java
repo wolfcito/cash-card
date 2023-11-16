c31eba69393b11e6e3032d306cb9208b01d3b0cd
Luis Fernando <guffenix@gmail.com>
2023-11-15 23:00:33 -0500

    feat: get endpoint findById implementation

--- onecard/src/main/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashController.java
+++ onecard/src/main/java/solutions/mundovirtual/cash_card/infraestructure/controller/CardCashController.java
@@ -3,6 +3,7 @@ package solutions.mundovirtual.cash_card.infraestructure.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solutions.mundovirtual.cash_card.domain.CashCard;
@@ -12,8 +13,12 @@ import solutions.mundovirtual.cash_card.domain.CashCard;
 public class CardCashController {
 
 	@GetMapping("/{requestedId}")
-	public ResponseEntity<CashCard> findById() {
-		CashCard cashCard = new CashCard(99L, 123.45);
-		return ResponseEntity.ok(cashCard);
+	public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+		if (requestedId.equals(99L)) {
+			CashCard cashCard = new CashCard(99L, 123.45);
+			return ResponseEntity.ok(cashCard);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
